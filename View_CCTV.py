6d43bc6adb0e398fb40d04c62f63c5f139976db9
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-02-16 11:25:51 +0900

    available youtube:

--- View_CCTV.py
+++ View_CCTV.py
@@ -2,20 +2,25 @@ import cv2
 import numpy as np
 import multiprocessing
 from config_hd_2 import cams
+import time
+import pafy
+
 
 def View_cam(cctv_name,cctv_addr):
+    
     cap = cv2.VideoCapture(cctv_addr)
-    print(f'CCTV : {cctv_name} now FPS{cap.get(cv2.CAP_PROP_FPS)}')
-    cap.set(cv2.CAP_PROP_FPS,1)
-    print(f'CCTV : {cctv_name} after set FPS{cap.get(cv2.CAP_PROP_FPS)}')
+    
     while True:
+        start_time  = time.time()
         ret,frame = cap.read()
+        end_time = time.time()
+        print(f'Get {cctv_name} frames - {round(end_time - start_time,3)} s')
 
         if ret:
             frame = cv2.resize(frame,dsize=(720,480))
             cv2.imshow(cctv_name,frame)
         else:
-            Error_image = np.zeros((720, 1280, 3), np.uint8)
+            Error_image = np.zeros((480, 720, 3), np.uint8)
             cv2.putText(Error_image, "Video Not Found!", (340, 240), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # 비디오 접속 끊어짐 표시
             cv2.imshow(cctv_name, Error_image)
             cap = cv2.VideoCapture(cctv_addr)
@@ -25,12 +30,19 @@ def View_cam(cctv_name,cctv_addr):
             cap.release()
             break
 
-def main():
+def main(): 
+    cctv_addrs=dict()
     works=[]
     jobs=[]
+    video=pafy.new("https://www.youtube.com/watch?v=nzvaWlpEbnc")
+    best=video.getbest(preftype="mp4")
+    cctv_addrs['YTN']=best.url
+    
 
-    for cctv_name in cams.keys():
-        works.append((cctv_name,cams[cctv_name]['src']))
+    # for cctv_name in cams.keys():
+    #     works.append((cctv_name,cams[cctv_name]['src']))
+    for cctv_name in cctv_addrs:
+        works.append((cctv_name,cctv_addrs[cctv_name]))
 
     for i, work in enumerate(works):
         p = multiprocessing.Process(target=View_cam, args=work)
